3e4ecf66be1d01e0dc8e9f08c105c503888d3de2
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-29 12:28:53 -0700

    New pull request

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,7 +1,4 @@
-// export async function getPokemon() {
-//     const data = await client.from('pokemon').select('*')
-//     return data.body;
-// }
+
 export async function getPokemon(filter) {
   const rawData = await fetch(`http://localhost:8888/.netlify/functions/pokemon?pokeQuery=${filter}`);
   const data = await rawData.json();
